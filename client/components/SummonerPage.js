a8859a9ebb98021d187f7083802634992988a9bf
Arthur Phung <rthrphung73@gmail.com>
2021-03-25 00:21:37 -0400

    BE + FE: Working game mode and champion no. for latest match

--- client/components/SummonerPage.js
+++ client/components/SummonerPage.js
@@ -1,14 +1,15 @@
 import axios from 'axios'
 import React from 'react'
-import {connect} from 'react-redux'
-// import fetchSummonerByName from '../store/summoner'
 
 export class SummonerPage extends React.Component {
   constructor() {
     super()
     this.state = {
       value: '',
-      accId: ''
+      accId: '',
+      gameId: '',
+      champion: 0,
+      gameMode: ''
     }
 
     this.handleChange = this.handleChange.bind(this)
@@ -16,17 +17,38 @@ export class SummonerPage extends React.Component {
     this.fetchSummonerByName = this.fetchSummonerByName.bind(this)
   }
 
-  // componentDidMount() {
-  //     this.props.loadSummonerByName()
-  // }
-
-  async fetchSummonerByName(event) {
-    this.setState({
-      value: event.target.value
-    })
+  async fetchSummonerByName() {
     try {
-      let {data} = await axios.get('/api/search/summoner')
-      console.log(data)
+      //Call API to get Summoner's Account ID
+      let {data} = await axios.get('/api/search/summoner', {
+        params: {username: this.state.value}
+      })
+      console.log('SUMMONER NAME DATA', data)
+
+      this.setState({
+        accId: data.accountId
+      })
+      //Call API to get Summoner's Game ID's
+      let matches = await axios.get('/api/search/matches', {
+        params: {accId: this.state.accId}
+      })
+      console.log('MATCHLIST DATA', matches)
+
+      let startIdx = matches.data.startIndex
+      this.setState({
+        gameId: matches.data.matches[startIdx].gameId,
+        champion: this.state.champion + matches.data.matches[startIdx].champion
+      })
+
+      //Call API to get Summoner's Match Details
+      let games = await axios.get('api/search/gameDetails', {
+        params: {gameId: this.state.gameId}
+      })
+      console.log('GAME DETAILS 1', games)
+
+      this.setState({
+        gameMode: games.data.gameMode
+      })
     } catch (error) {
       console.log(error)
     }
@@ -39,41 +61,29 @@ export class SummonerPage extends React.Component {
   }
 
   handleSubmit(event) {
+    this.fetchSummonerByName()
     event.preventDefault()
   }
 
   render() {
-    console.log('PROPS HERE', this.props)
-    console.log('STATE HERE', this.state)
-    console.log(this.props.loadSummonerByName)
+    // console.log('PROPS HERE', this.props)
+    // console.log('STATE HERE', this.state)
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label>
           Summoner Name
           <input
             type="text"
             value={this.state.value}
-            onChange={this.fetchSummonerByName}
+            onChange={this.handleChange}
           />
         </label>
         <input type="submit" value="Submit" />
+        <h1>{this.state.gameMode}</h1>
+        <h1>{this.state.champion}</h1>
       </form>
     )
   }
 }
 
-const mapState = state => {
-  return {
-    summonerData: state.summoner
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    loadSummonerByName: () => {
-      dispatch(fetchSummonerByName())
-    }
-  }
-}
-
-export default connect(mapState, mapDispatch)(SummonerPage)
+export default SummonerPage
--- server/api/search.js
+++ server/api/search.js
@@ -1,16 +1,52 @@
 const router = require('express').Router()
 const axios = require('axios')
+require('dotenv').config()
 
-const riotKey = 'RGAPI-c8830881-4d8c-4247-9fec-c9ca5ebcab46'
+const riotKey = process.env.API_KEY
 
 module.exports = router
 
 //Get Summoner by name
 router.get('/summoner', async (req, res, next) => {
   try {
-    const response = await axios.get(
-      'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/holobi?api_key=RGAPI-c8830881-4d8c-4247-9fec-c9ca5ebcab46'
-    )
+    let {username} = req.query
+    console.log(riotKey, 'TESTESTETSET')
+    console.log('username here ==>', username)
+    let url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${riotKey}`
+
+    const response = await axios.get(url)
+    let data = response.data
+    res.json(data)
+    console.log(data)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//Get Match-list for games played on given account ID
+router.get('/matches', async (req, res, next) => {
+  try {
+    let {accId} = req.query
+    console.log('account id here ==>', accId)
+    let url = `https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${accId}?api_key=${riotKey}`
+
+    const response = await axios.get(url)
+    let matches = response.data
+    res.json(matches)
+    console.log(matches)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//Get Game Details for specific matches
+router.get('/gameDetails', async (req, res, next) => {
+  try {
+    let {gameId} = req.query
+    console.log('game id here ==>', gameId)
+    let url = `https://na1.api.riotgames.com/lol/match/v4/matches/${gameId}?api_key=${riotKey}`
+
+    const response = await axios.get(url)
     let data = response.data
     res.json(data)
     console.log(data)
