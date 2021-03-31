098bc16dac36e5f09cab78fbd9e7d7b4f51ecfe2
SeungYeun Lee <sylee538@bu.edu>
2021-03-31 16:12:58 -0400

    FE: Navbar created

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,19 +1,31 @@
 import React from 'react'
 
 import Nav from 'react-bootstrap/Nav'
+import Navbarb from 'react-bootstrap/Navbar'
 
 const Navbar = () => (
   <div>
-    <h1>LoL Stats</h1>
-    <Nav defaultActiveKey="/home" as="ul">
-      <Nav.Item as="li">
+    <Navbarb bg="dark" variant="dark">
+      <Navbarb.Brand href="#home">
+        <img
+          alt=""
+          src="/poro.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+        LoL Stats
+      </Navbarb.Brand>
+    </Navbarb>
+    <Nav fill variant="tabs" defaultActiveKey="/home">
+      <Nav.Item>
         <Nav.Link href="/">Home</Nav.Link>
       </Nav.Item>
-      <Nav.Item as="li">
+      <Nav.Item>
         <Nav.Link href="/about">About</Nav.Link>
       </Nav.Item>
-      <Nav.Item as="li">
-        <Nav.Link href="/champion">Champion</Nav.Link>
+      <Nav.Item>
+        <Nav.Link href="/champions">Champions</Nav.Link>
       </Nav.Item>
     </Nav>
   </div>
