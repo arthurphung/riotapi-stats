4cfd2ace81b4632f83b1ad8341eb313f0ac73932
Arthur Phung <rthrphung73@gmail.com>
2021-04-09 02:15:48 -0400

    FE: navbar revision

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
 import Nav from 'react-bootstrap/Nav'
+import NavbarBS from 'react-bootstrap/Navbar'
 
 const Navbar = () => (
-  <div>
-    <Nav justify variant="tabs" defaultActiveKey="/home">
+  <>
+    <NavbarBS bg="primary" variant="dark">
+      <NavbarBS.Brand href="/">
+        <img
+          src="/poroLogo.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="Porolytix logo"
+        />
+      </NavbarBS.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
+      </Nav>
+    </NavbarBS>
+    {/* <Nav justify variant="tabs" defaultActiveKey="/home">
       <Nav.Item>
         <Nav.Link href="/">Home</Nav.Link>
       </Nav.Item>
@@ -14,8 +30,8 @@ const Navbar = () => (
       <Nav.Item>
         <Nav.Link href="/champions">Champions</Nav.Link>
       </Nav.Item>
-    </Nav>
-  </div>
+    </Nav> */}
+  </>
 )
 
 export default Navbar
